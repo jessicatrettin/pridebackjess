e5e880d81af0d2be83875d79f457a27f1e1f7fb8
jessica.trettin@gmail.com <jessica.trettin@gmail.com>
2021-10-06 21:58:53 -0300

    Finalmente foi

--- Pride+/Domain/ProfessionalType.cs
+++ Pride+/Domain/ProfessionalType.cs
@@ -8,13 +8,13 @@ namespace Pride_.Domain
 {
     public class ProfessionalType
     {
-        [Required]
         public static void Main()
         {
-            string[] myStringArray = new string[7] { "Médico", "Dentista", "Psicólogo", "Nutricionista", "Enfermeira", "Fisioterapeuta", "Outro"  };
+            string[] profissionalType = { "Médico", "Dentista", "Psicólogo", "Nutricionista", "Enfermeira", "Fisioterapeuta", "Outro" };
 
-            
+
             Object[] myObjArray = new Object[5] { 26, 27, 28, 29, 30 };
 
         }
     }
+}
